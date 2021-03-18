f25fed97a1ac159f1627d09fed7cee94e7d36288
lakashmaharjan <lakashmaharjan@gmail.com>
2021-03-18 20:28:49 +0545

    button added, Task-2 add fragment dinamically

--- app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
+++ app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2017 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.fragmentexample;
 
 import android.os.Bundle;
@@ -22,39 +6,47 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioGroup;
-import android.widget.RatingBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
-
-
-
+/**
+ * A simple {@link Fragment} subclass that shows a question
+ * with radio buttons for providing feedback. If the user
+ * clicks "Yes" the text header changes to "Article: Like".
+ * If the user clicks "No" the text header changes to "Thanks".
+ */
 public class SimpleFragment extends Fragment {
 
-
-    public static final int YES = 0;
-    public static final int NO = 1;
+    // The radio button choice has 3 states: 0 = yes, 1 = no,
+    // 2 = default (no choice). Using only 0 and 1.
+    private static final int YES = 0;
+    private static final int NO = 1;
 
     public SimpleFragment() {
-
+        // Required empty public constructor
     }
 
+    /**
+     * Creates the view for the fragment.
+     *
+     * @param inflater           LayoutInflater to inflate any views in the fragment
+     * @param container          ViewGroup of parent view to attach fragment
+     * @param savedInstanceState Bundle for previous state
+     * @return rootView
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+        // Inflate the layout for this fragment.
         final View rootView = inflater.inflate(R.layout.fragment_simple,
                 container, false);
         final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
-        final RatingBar ratingBar =
-                rootView.findViewById(R.id.ratingBar);
 
         // Set the radioGroup onCheckedChanged listener.
         radioGroup.setOnCheckedChangeListener(
                 new RadioGroup.OnCheckedChangeListener() {
                     @Override
-                    public void onCheckedChanged(RadioGroup group,
-                                                 int checkedId) {
+                    public void onCheckedChanged(RadioGroup group, int checkedId) {
                         View radioButton = radioGroup.findViewById(checkedId);
                         int index = radioGroup.indexOfChild(radioButton);
                         TextView textView =
@@ -73,20 +65,11 @@ public class SimpleFragment extends Fragment {
                     }
                 });
 
-        // Set the rating bar onCheckedChanged listener.
-        ratingBar.setOnRatingBarChangeListener
-                (new RatingBar.OnRatingBarChangeListener() {
-                    @Override
-                    public void onRatingChanged(RatingBar ratingBar,
-                                                float rating, boolean fromUser) {
-                        // Get rating and show Toast with rating.
-                        String myRating = (getString(R.string.my_rating) +
-                                String.valueOf(ratingBar.getRating()));
-                        Toast.makeText(getContext(), myRating,
-                                Toast.LENGTH_SHORT).show();
-                    }
-                });
-
+        // Return the View for the fragment's UI.
         return rootView;
+    }
+
+    public static SimpleFragment newInstance() {
+        return new SimpleFragment();
     }
 }
